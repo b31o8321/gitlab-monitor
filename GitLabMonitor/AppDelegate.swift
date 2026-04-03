e1787b52df896ed5cd5f455de38ea51c14607937
yiqian.zhang <yiqian.zhang@shulex-tech.com>
2026-04-03 13:58:04 +0800

    chore: init Xcode project with LSUIElement

---
+++ GitLabMonitor/AppDelegate.swift
@@ -0,0 +1,7 @@
+import AppKit
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Menu bar setup will be added in Task 7
+    }
+}
